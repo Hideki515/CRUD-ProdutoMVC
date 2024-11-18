0e6e54dc5bb99c44a8cb662da21de11cb17e2c36
Bruno Hideki Sato Misufara <61321130+Hideki515@users.noreply.github.com>
2024-11-18 10:49:41 -0300

    Add initial project structure with error handling and basic routing

---
+++ App/Views/layouts/footer.php
@@ -0,0 +1,13 @@
+    <footer class="footer mt-auto py-3">
+      <div class="container">
+        <span class="text-muted">### Rodapé.</span>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="http://<?php echo APP_HOST; ?>/public/js/jquery-3.5.1.min.js"></script> 
+    <script src="http://<?php echo APP_HOST; ?>/public/js/bootstrap.bundle.min.js"></script>
+       
+  </body>
+</html>
